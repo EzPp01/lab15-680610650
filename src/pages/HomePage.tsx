d00bab517305eebb16cd42e84b219665a6fe71ab
Krittapat <kittapat302549@gmail.com>
2026-09-23 17:08:25 +0700

    a

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function HomePage() {
-  return <></>;
+  return (
+    <Card className="w-fit">
+      <CardHeader>
+        <CardTitle>ระบบลงทะเบียนเรียน CPE & ISNE</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button render={<Link to="/enrollment" />}>ไปหน้าลงทะเบียนเรียน</Button>
+      </CardContent>
+    </Card>
+  );
 }
