5b258ba702ba9158e3fddd089e94d9c3e6589e39
Krittapat <kittapat302549@gmail.com>
2026-09-23 17:12:22 +0700

    k

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,15 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function HomePage() {
   return (
-    <Card className="w-fit">
-      <CardHeader>
-        <CardTitle>ระบบลงทะเบียนเรียน CPE & ISNE</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Button render={<Link to="/enrollment" />}>ไปหน้าลงทะเบียนเรียน</Button>
-      </CardContent>
-    </Card>
+    <div className="flex justify-center pt-8">
+      <Card className="w-fit">
+        <CardHeader>
+          <CardTitle>ระบบลงทะเบียนเรียน CPE & ISNE</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button render={<Link to="/enrollment" />}>ไปหน้าลงทะเบียนเรียน</Button>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
